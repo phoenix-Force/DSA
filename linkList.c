938fd1318edb68d1f4714b58d72c1787147fee71
ppppsky247 <ppppono29@gmail.com>
2026-02-08 03:11:21 +0530

    sngly linked list

--- linkList.c
+++ linkList.c
@@ -4,21 +4,45 @@
 struct node {
     int data;
     struct node * next;
+};
+
+struct node* head = NULL;
+
+void insert(int num) {
+    struct node * temp = malloc(sizeof(struct node));
+    temp->data = num;
+    temp->next=NULL;
+
+    
+    if(head==NULL) {
+        head=temp;
+        return;
+    }
+    struct node* current = head;
+    while(current->next != NULL) {
+        current = current->next;
+    }
+    current->next = temp;
+
 }
 
-void traverse(struct node * head
+void traverse(struct node * begginging)
 {
-    struct node* current = head;
+    struct node* current = begginging;
 
     while(current != NULL) {
-        printf("%d", current->data);
-        current=current-> next;
+        printf("%d --->", current->data);
+        current=current->next;
     }
 };
-)
+
+
 
 
 int main() {
-    scruct node 
-    return 0
+    insert(10);
+    insert(20);
+    insert(30);
+    traverse(head);
+    return 0;
 }
